fb3ed4f7289f49f0222fcc5bc6acfea6133f9d28
Yush Pokharel <github.com/Yush-Pokharel>
2022-08-20 14:25:45 +0545

    working functionality made and finished

--- data_structures.c
+++ data_structures.c
@@ -3,10 +3,10 @@
 #include<string.h>
 #define MAX 20
 
-struct stack(){
+struct stack{
     int top;
     char array[MAX];
-}
+};
 
 void push(stack *s,char n){
     s->top += 1;
@@ -20,6 +20,12 @@ char pop(stack *s){
     return poped;
 }
 
+void traverse(stack *s){
+    for( int i = 0; i <= s->top; i++){
+        printf("%c",s->array[i]);
+    }   
+}
+
 void clear(stack *s){
     s->top = -1;
 }
@@ -28,46 +34,74 @@ char peek(stack *s){
     return (s->array[s->top]);
 }
 
+bool isEmpty(stack *s){
+    if(s->top == -1){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
 int precedence(char n){
     if (n == '(')
-        return 5;
-    else if(n == "^")
+        return 1;
+    else if(n == '^')
         return 4;
-    else if(n == "/" || n =="*")
+    else if(n == '/' || n =='*')
         return 3;
-    else if(n == "+" || n =="-")
+    else if(n == '+' || n =='-')
         return 2;
     else
         return 0;
 }
 
 int main(){
-    char array[MAX], operands[MAX], top_value;
-    stack st;
-    int j;
-    printf("enter the infix: \n");
+    stack post_stack, op_stack;
+    clear(&post_stack);
+    clear(&op_stack);
+    char array[MAX];
+    printf("enter the infix expression: \n");
     gets(array);
-    clear(&st);
-    for( int i = 0, j ; i < MAX; i++){
-        if(isalnum(array[i]) == 1){
-            operands[j] = array[i];
-            j++;
+    for(int i = 0; i < strlen(array); i++){
+        if (array[i] == '('){
+            push(&op_stack,array[i]);
         }
-        else{
-            if(array[i] == '('){
-                push(&st,array[i]);
-            }
-            else{
-                top_value = peek(&st);
-                if(isalnum(top_value)==0){
-
+        else if(isalpha(array[i])){
+            push(&post_stack,array[i]);
+        }
+        else if( precedence(array[i]) > 1){
+            char top = peek(&op_stack);
+            if(precedence(top)>1 ){
+                char top_p = precedence(top);
+                char array_p = precedence(array[i]);
+                if(top_p < array_p){
+                    push(&op_stack,array[i]);
+                }
+                else if(top_p >= array_p){
+                    char receive = pop(&op_stack);
+                    push(&post_stack, receive);
+                    push(&op_stack,array[i]);
                 }
             }
-            while( precedence(st.array[st.top]) > precedence(array[i])){
-                c = pop(&st);
-                operands[j] = c;
-                j++;
+            else{
+                push(&op_stack,array[i]);
+            }
+        }
+        else if (array[i] == ')'){
+            while(op_stack.array[op_stack.top] != '('){
+                char get = pop(&op_stack);
+                push(&post_stack,get);
+            }
+            if(op_stack.array[op_stack.top] == '('){
+                char discard = pop(&op_stack);
             }
         }
     }
+    while(op_stack.top != -1){
+        char obtained = pop(&op_stack);
+        push(&post_stack,obtained);
+    }
+    printf("postfix is: \n");
+	traverse(&post_stack);
 }
